558ff03be131a9b0d431d17d9b86aab01a7d7f43
syamphilip <syamphilip96@gmail.com>
2021-11-02 23:42:17 +0530

    custom search

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const http = require("http");
+const url = require('url');
 const getGeo = require("./geoCoding/geo");
 const getWeather = require("./getWeather/getWeather");
 
 let server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    return getGeo("kottayam", (body) => {
-      getWeather(body, (data) => {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(data);
+  const queryObject = url.parse(req.url, true).query;
+  if (req.url === `/search?location=${queryObject.location}`) {
+    if (queryObject.location) {
+      return getGeo(queryObject.location, (body) => {
+        getWeather(body, (data) => {
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(data);
+        });
       });
-    });
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("location is missing ");
+  } else {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("soon active");
   }
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end("vera url ane\n");
+
 });
 
 server.listen(process.env.PORT || 3000);
