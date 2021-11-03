f0a9eb35b93a421d23826032aa93d79f83027b33
syamphilip <syamphilip96@gmail.com>
2021-11-03 21:15:20 +0530

    mongoose

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const http = require("http");
 const url = require('url');
 const getGeo = require("./geoCoding/geo");
 const getWeather = require("./getWeather/getWeather");
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const WeatherSchema = new Schema({
+  weatherLocation: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true })
+
+const WeatherData = mongoose.model("LocationHistory", WeatherSchema);
+
+const connectionURL =
+  "mongodb+srv://syamphilip96:syamphilip96@cluster0.oeec8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log("Connected to DB");
+  server.listen(process.env.PORT || 3000);
+})
 
 let server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url, true).query;
@@ -9,6 +29,10 @@ let server = http.createServer((req, res) => {
     if (queryObject.location) {
       return getGeo(queryObject.location, (body) => {
         getWeather(body, (data) => {
+          const newWeatherHistory = new WeatherData({
+            weatherLocation: queryObject.location
+          })
+          newWeatherHistory.save();
           res.writeHead(200, { "Content-Type": "application/json" });
           res.end(data);
         });
@@ -20,7 +44,6 @@ let server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("soon active");
   }
-
 });
 
-server.listen(process.env.PORT || 3000);
+
